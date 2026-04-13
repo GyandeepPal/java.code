c7ce932108a03b67cdf8ba0ebdcfb0478994856f
Gyandeep Pal <gyandeeppal577@gmail.com>
2026-04-13 11:00:40 +0530

    ff

---
+++ DigitCount.java
@@ -0,0 +1,14 @@
+public class DigitCount {
+    static int digitofcount(int n){
+        if(n == 0){
+            return 0;
+        }
+        return (n % 10 + digitofcount(n / 10));
+    }
+
+    public static void main(String[] args){
+        int n = 1234;
+        System.out.println(digitofcount(n));
+    }
+}
+
