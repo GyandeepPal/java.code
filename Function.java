ff1b3e1bf5d52e6ccabf8fac47f8d132ff02b2f3
GyandeepPal <gyandeeppal577@gmail.com>
2026-04-19 18:19:52 +0530

    function

---
+++ Function.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Function {
+    public static void printMyName(String Name){
+        System.out.println(Name);
+        return;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String name = sc.next();
+        printMyName(name);
+    }
+}
