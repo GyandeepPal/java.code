d4d72b55d69a76b17850c0274c53d667514a2e2e
GyandeepPal <gyandeeppal577@gmail.com>
2026-04-24 11:18:40 +0530

    TwoDArray

---
+++ Array/Names.java
@@ -0,0 +1,24 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Names {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size=sc.nextInt();
+        String std[]=new String [size];
+
+
+        // input
+        for(int i=0; i<size; i++){
+            std[i] = sc.next();
+        }
+
+        //output
+
+        for(int i=0; i<std.length; i++){
+            System.out.println(i+1);
+            System.out.println(std[i]);
+        }
+    }
+}
