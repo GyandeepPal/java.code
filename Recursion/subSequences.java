f584ac2b2baefea8d7d9c39c1d2db68fcb50512b
GyandeepPal <gyandeeppal577@gmail.com>
2026-05-06 22:40:51 +0530

    keypad

---
+++ Recursion/subSequences.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class subSequences {
+    public static void subSequences(String str, int idx, String newString){
+        if (idx == str.length()){
+            System.out.println(newString);
+            return;
+        }
+        char currchar = str.charAt(idx);
+        //to be
+        subSequences(str, idx+1, newString+currchar);
+        // to not be
+        subSequences(str, idx+1, newString);
+    }
+    public static void main(String[] args) {
+        String str = "abc";subSequences(str, 0, " ");
+
+    }
+}
---
+++ Recursion/uniqueSubSequences.java
@@ -0,0 +1,38 @@
+package Recursion;
+
+import java.util.HashSet;
+
+
+
+public class uniqueSubSequences {
+    public static void subSequences(String str, int idx, String newString, HashSet<String> set){
+
+
+        if (idx == str.length()){
+            if(set.contains(newString)){
+                return;
+            }else{
+                System.out.println(newString);
+                set.add(newString);
+                return;
+            }
+           
+           
+        }
+        char currchar = str.charAt(idx);
+        //to be
+        subSequences(str, idx+1, newString+currchar,set);
+        // to not be
+        subSequences(str, idx+1, newString, set);
+
+
+    }
+    public static void main(String[] args) {
+        String str = "aaa";
+        HashSet<String> set = new HashSet<>();
+        subSequences(str, 0, " ", set);
+
+    }
+}
+
+
