0a459f70280c15e8f54a01eee3d13a0401781a8a
GyandeepPal <gyandeeppal577@gmail.com>
2026-05-06 22:48:01 +0530

    main

---
+++ Recursion/hello.java
@@ -0,0 +1,12 @@
+package Recursion;
+
+public class hello {
+    public static void printComb(String str, int num){
+        int n=12;
+        System.out.println(n);
+    }
+    public static void main(String[] args) {
+        String str = "ram";
+        printComb(str,2);
+    }
+}
