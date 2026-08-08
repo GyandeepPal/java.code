8fd0393d2e184022c22b209d5337236eccbbd8df
GyandeepPal <gyandeeppal577@gmail.com>
2026-08-08 20:11:36 +0530

    index

--- Array/index.java
+++ Array/index.java
@@ -1,6 +1,10 @@
 public class index {
     public static void main(String[] args) {
-       int Array[]={23,4,5,6,7};
+    //    int Array[]={23,4,5,6,7};
+    String Array="gyan deep pal 🙈";
+    char news = 'a';
+       System.out.println(Array);
        
+       System.out.println(news);
     }
 }
