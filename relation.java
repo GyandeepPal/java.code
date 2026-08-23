f2a7c750879a5bae1d6dba6c39fe4cf371c7e52f
GyandeepPal <gyandeeppal577@gmail.com>
2026-08-23 19:43:18 +0530

    reeiational operators

---
+++ relation.java
@@ -0,0 +1,21 @@
+public class relation {
+   public static void main(String[] args) {
+      int a = 33;
+      int c = 33;
+
+
+      boolean b = (a == c);
+      System.out.println(b); //true
+
+      
+      boolean s = (a != c);
+      System.out.println(s); //false
+
+
+      boolean e=(a<c);
+      boolean f=(a<=c);
+      System.out.println(e);//false
+      System.out.println(f);//true
+
+   }
+}
