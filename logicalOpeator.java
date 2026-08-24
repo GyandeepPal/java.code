77c575b6ca44958c890c00e116331e70799b23b2
GyandeepPal <gyandeeppal577@gmail.com>
2026-08-24 18:44:25 +0530

    Operators

---
+++ logicalOpeator.java
@@ -0,0 +1,23 @@
+public class logicalOpeator {
+    public static void main(String[] args) {
+
+        // &&  (and) opretor
+
+        int a =22;
+        int b =34;
+        int c =32;
+        int d =322;
+        boolean e =(a<b ) && (c<d); 
+        System.out.println(e);  //true
+
+        // || (or) operator
+
+        int f = 22;
+        int g = 34;
+        int h = 32;
+        int i = 322;
+        boolean j = (f < g) || (h > i); 
+        System.out.println(j);    //true
+
+    }
+}
